6c10c6ac455cc364379a64c3789daeafe5df2423
mateshwari <ganesh.medidi@think.design>
2024-02-07 00:09:46 +0530

    added cors issue

--- index.js
+++ index.js
@@ -2,29 +2,32 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
-
 import Connection from './database/db.js';
-import DefaultData from './default.js';
 import Routes from './routes/route.js';
-
 
 dotenv.config();
 const app = express();
 
-const PORT = process.env.PORT ||  8000;
+const PORT = process.env.PORT || 8000;
 const db = process.env.DB_URI;
 Connection(db);
 
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// CORS configuration
 app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Allow requests from all origins
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 app.get('/', (req, res) => {
-    res.send('flipkart clone products api running');
+  res.send('flipkart clone products API running');
 });
+
 app.use('/api', Routes);
+
 app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
-//DefaultData();
-
-
-
